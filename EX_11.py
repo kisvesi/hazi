32f9ae70efd12ea1ed12433fd80bbb6cf285b613
kisvesi <36821387+kisvesi@users.noreply.github.com>
2018-04-29 09:48:59 +0200

    Update EX_11.py

--- EX_11.py
+++ EX_11.py
@@ -1,17 +1,14 @@
 n = 0
-i = 999
-j = 999
+x = 0
+y = 0
 
-while n == 0 and i > 99:
-    j = i
-    while n == 0 and j > 99:
+for i in range(999, 99, -1):
+    for j in range(i, 99, -1):
         szorzat = str(i * j)
         if int(szorzat) > n:
             if szorzat == szorzat[::-1]:
                 n = int(szorzat)
                 x = i
                 y = j
-        j -= 1
-    i -= 1
 
-print(n, ' a legnagyobb palindromszám, amely kettő 3-jegyű szám (', x, ' x ', y, ') szorzataként felírható.')
+print(n, 'a legnagyobb palindromszám, amely kettő 3-jegyű szám (', x, 'x', y, ') szorzataként felírható.')
