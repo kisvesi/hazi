f7cf7b6a9ba0c5c38bbae33c3b296f9676f2fc38
kisvesi <36821387+kisvesi@users.noreply.github.com>
2018-05-01 20:55:06 +0200

    Add files via upload

---
+++ EX_2.py
@@ -0,0 +1,9 @@
+# def atvaltas(n, b1, b2):
+#
+#
+
+szam, szr1 = input('Adjon meg egy számot és annak számrendszerét!: ')
+szr2 = int(input('Adja meg hanyas számrendszerbe szeretné átváltani!: '))
+print(szam)
+print(szr1)
+print(szr2)
