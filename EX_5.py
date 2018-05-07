e506641e3c15c5de360f3cabe47869f94e6752c3
kisvesi <36821387+kisvesi@users.noreply.github.com>
2018-05-07 16:15:08 +0200

    Add files via upload

--- EX_5.py
+++ EX_5.py
@@ -1,5 +1,36 @@
 dict = {0: '', 1: '-', 2: '+'}
 
+
+def kiszamol(ls):
+    for i in ls:
+        ossz = 0
+        aktszam = ''
+        aktelojel = ''
+        for j in i:
+            if '1' <= j <= '9':
+                aktszam += j
+            else:
+                if aktelojel == '':
+                    ossz = int(aktszam)
+                    aktszam = ''
+                    aktelojel = j
+                else:
+                    if aktelojel == '+':
+                        ossz += int(aktszam)
+                        aktszam = ''
+                        aktelojel = j
+                    else:
+                        ossz -= int(aktszam)
+                        aktszam = ''
+                        aktelojel = j
+        if aktelojel == '+':
+            ossz += int(aktszam)
+        else:
+            ossz -= int(aktszam)
+        if ossz == 100:
+            print(i,'= 100')
+
+
 sorok = []
 
 for a in range(3):
@@ -13,3 +44,5 @@ for a in range(3):
                                 sor = '1'
                                 sor += dict[a] + '2' + dict[b] + '3' + dict[c] + '4' + dict[d] + '5' + dict[e] + '6' + dict[f] + '7' + dict[g] + '8' + dict[h] + '9'
                                 sorok.append(sor)
+
+kiszamol(sorok)
